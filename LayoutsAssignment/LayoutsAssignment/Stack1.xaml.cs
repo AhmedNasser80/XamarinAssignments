9c0ed2136ae7e31cfe0df4443c6e68695da00cc8
AhmedNasser80 <ahnma600@gmail.com>
2022-08-21 15:29:21 +0200

    initial Create

---
+++ LayoutsAssignment/LayoutsAssignment/Stack1.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LayoutsAssignment
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Stack1 : ContentPage
+    {
+        public Stack1()
+        {
+            InitializeComponent();
+            Button btn = new Button();
+            btn.Clicked += Btn_Clicked;
+            btn.Text = "Stack2";
+            btn.Margin = new Thickness(0, 20, 0, 0);
+            StackLayout stackLayout = new StackLayout
+            {
+                Spacing = 0,
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+                Children =
+
+                {
+                    new Label
+                    {
+                        Text = "Welcome to Xamarin",
+                        HorizontalOptions = LayoutOptions.Center,
+                        FontSize = 24
+                    },
+                    new Label
+                    {
+                        Text = "Cross-platform apps made easy",
+                        HorizontalOptions = LayoutOptions.Center,
+                        FontSize = 20
+
+
+                    },
+                    new Label
+                    {
+                        Text = "Login",
+                        HorizontalOptions = LayoutOptions.FillAndExpand,
+                        HorizontalTextAlignment = TextAlignment.Center,
+                        BackgroundColor = Color.Navy,
+                        Margin = new Thickness(0, 10),
+
+
+                    },
+                    new Label
+                    {
+                        Text = "Register",
+                        HorizontalOptions = LayoutOptions.FillAndExpand,
+                        HorizontalTextAlignment = TextAlignment.Center,
+                        BackgroundColor = Color.Navy,
+                        Margin = new Thickness(0, 10),
+                    },
+                    btn
+
+                }
+
+            };
+
+            Content = stackLayout;
+
+        }
+
+        private async void Btn_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Stack2());
+        }
+    }
+ }
---
+++ LayoutsAssignment/LayoutsAssignment/Stack2.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LayoutsAssignment
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Stack2 : ContentPage
+    {
+        public Stack2()
+        {
+            InitializeComponent();
+            StackLayout stackLayout = new StackLayout
+            {
+                Spacing = 0,
+                Margin = new Thickness(10, 20, 10, 20),
+                Children =
+            {
+                new Image
+                {
+                    Source="https://aka.ms/campus.jpg",
+                    HorizontalOptions= LayoutOptions.FillAndExpand,
+
+                },
+                new StackLayout
+                {
+                    Orientation=StackOrientation.Horizontal,
+                    Children =
+                    {
+                        new Label
+                        {
+                            Text="Share",
+                            TextColor=Color.Navy
+                        },
+                         new Label
+                        {
+                            Text="commit",
+                            TextColor=Color.Navy
+                        },
+                          new Label
+                        {
+                            Text="like",
+                            TextColor=Color.Navy
+                        }
+
+
+                    }
+
+                },
+
+                new Label
+                {
+                    Text = "700 likes",
+                    FontSize=18,
+                    FontAttributes= FontAttributes.Bold
+                },
+                new Label
+                {
+                    Text = "this is shot Yesterday"
+
+                },
+            }
+            };
+            Content = stackLayout;
+        }
+    }
+}
